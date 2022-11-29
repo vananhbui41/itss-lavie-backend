18b31e8a3f417ff01fb920a1ed0d563f53b2cfc4
Bui Van Anh <anhbv@bsscommerce.com>
2022-11-29 19:59:57 +0700

    Update API:register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Validation\Validator;
+use Validator;
 
 class AuthController extends Controller
 {
