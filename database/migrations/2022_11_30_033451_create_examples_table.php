8a0115b79ef686643062403cb8bfb3893e9ba2c0
hungdx85014 <hungdx85014@co-well.vn>
2022-11-30 11:00:34 +0700

    migration

---
+++ database/migrations/2022_11_30_033451_create_examples_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamplesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examples', function (Blueprint $table) {
+            $table->id();
+            $table->integer('exampleId');
+            $table->integer('wordId');
+            $table->integer('tagId');
+            $table->string('image');
+            $table->string('meaning');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examples');
+    }
+}
