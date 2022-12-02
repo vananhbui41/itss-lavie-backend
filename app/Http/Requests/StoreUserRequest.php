c16c33cbcb4e46624c8eb75e6324508c0e9e492f
Bui Van Anh <anhbv@bsscommerce.com>
2022-12-03 00:32:26 +0700

    Update API-register email validate

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -25,7 +25,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255', 'unique:users,email'],
+            'email' => ['required', 'string', 'max:255', 'unique:users,email', 'email'],
             'password' => ['required', 'confirmed']
         ];
     }
